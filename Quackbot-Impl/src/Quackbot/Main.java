4b56c88b0f0d6926dd5df3bc3a564006cbfc4733
Leon Blakey <lord.quackstar@gmail.com>
2010-05-23 15:14:25 +0000

    Now can set Log level from GUI,
    GUI
    -New combo box for selecting level
    -Some handling in actionPreformed added
    -Root logger configured instead of package
    
    Main
    -Now reads db info from uncommited file
    
    Moved JRebel file around to impl lib since that's what it is

--- Quackbot-Impl/src/Quackbot/Main.java
+++ Quackbot-Impl/src/Quackbot/Main.java
@@ -6,6 +6,10 @@ package Quackbot;
 
 import Quackbot.plugins.JavaPlugin;
 import Quackbot.plugins.impl.JavaTest;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -14,10 +18,25 @@ import Quackbot.plugins.impl.JavaTest;
 public class Main {
 	public static void main(String[] args) {
 		Controller ctrl = new Controller();
-		ctrl.connectDB("quackbot", 10, "com.mysql.jdbc.Driver", "jdbc:mysql://localhost/quackbot", null, null, "root", null);
+		String[] dbInfo = getDBInfo();
+		ctrl.connectDB(dbInfo[0], 10, "com.mysql.jdbc.Driver", dbInfo[1], null, null, dbInfo[2], dbInfo[3]);
 		ctrl.setDatabaseLogLevel(java.util.logging.Level.OFF);
 		ctrl.addPlugin(new JavaPlugin(JavaTest.class.getName()));
 		//ctrl.addPlugin(new JavaPlugin(HookTest.class.getName()));
 		ctrl.start();
+	}
+
+	/**
+	 * Get all DB info from uncommited file. So no, you can't get access to my database :-)
+	 * @return String array with values: databasename, db connection string, username, and password
+	 */
+	public static String[] getDBInfo() {
+		try {
+			return new BufferedReader(new FileReader("mysqlPasswords.txt")).readLine().split(",");
+		}
+		catch(Exception e) {
+			LoggerFactory.getLogger(Main.class).error("Cannot find mysqlPasswords.txt in dir "+(new File("").getAbsolutePath()),e);
+		}
+		return null;
 	}
 }
