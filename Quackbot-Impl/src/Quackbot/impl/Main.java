934dd7c857406bdf994f1649ff22aa43781f2514
Leon Blakey <lord.quackstar@gmail.com>
2010-05-29 04:27:11 +0000

    Updated copyright notice and header's with GPL notice
    Documented all Java Plugin annotations
    
    Various other minor changes

--- Quackbot-Impl/src/Quackbot/impl/Main.java
+++ Quackbot-Impl/src/Quackbot/impl/Main.java
@@ -1,9 +1,27 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @(#)Main.java
+ *
+ * Copyright Leon Blakey/Lord.Quackstar, 2009-2010
+ *
+ * This file is part of Quackbot
+ *
+ * Quackbot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Quackbot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Quackbot.  If not, see <http://www.gnu.org/licenses/>.
  */
-package Quackbot;
 
+package Quackbot.impl;
+
+import Quackbot.Controller;
 import Quackbot.plugins.JavaPlugin;
 import Quackbot.plugins.impl.JavaTest;
 import java.io.BufferedReader;
--- Quackbot-Plugins/src/Quackbot/plugins/java/JavaBase.java
+++ Quackbot-Plugins/src/Quackbot/plugins/java/JavaBase.java
@@ -1,7 +1,22 @@
 /**
  * @(#)JavaBase.java
  *
+ * Copyright Leon Blakey/Lord.Quackstar, 2009-2010
+ *
  * This file is part of Quackbot
+ *
+ * Quackbot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Quackbot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Quackbot.  If not, see <http://www.gnu.org/licenses/>.
  */
 package Quackbot.plugins.java;
 
@@ -10,16 +25,19 @@ import Quackbot.Bot;
 import Quackbot.info.BotEvent;
 
 /**
- * Java command interface. All Java commands MUST implement this
+ * This is the Java Plugin interface that all Java Plugins <u>must</u> implement.
+ * 
  * @author Lord.Quackstar
  */
 public interface JavaBase {
 	/**
 	 * Called when spefic command is requested
 	 *
-	 * Commands MUST override this in order to work
 	 * @param bot     Bot instance
 	 * @param msgInfo BotEvent bean
+         *
+         * @throws Exception Any exception encoutered in execution that prevents
+         *                   plugin from continuing to execute
 	 */
 	public void invoke(Bot bot, BotEvent msgInfo) throws Exception;
 }
