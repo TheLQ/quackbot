c6a8a676ecbd389bc57e70e9bf44e5cc6310bb72
Leon Blakey <lord.quackstar@gmail.com>
2011-03-06 16:27:07 -0500

    New method in DataStore interface: getServers

--- core/src/main/java/org/quackbot/data/DataStore.java
+++ core/src/main/java/org/quackbot/data/DataStore.java
@@ -1,6 +1,8 @@
 
 package org.quackbot.data;
 
+import java.util.Set;
+
 /**
  *
  * @author Leon Blakey <lord.quackstar at gmail.com>
@@ -9,4 +11,5 @@ public interface DataStore {
 	public AdminStore newAdminStore(String name);
 	public ChannelStore newChannelStore(String name);
 	public ServerStore newServerStore(String address);
+	public Set<ServerStore> getServers();
 }
