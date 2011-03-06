245a26df41169b58e4d0383fa931b686332dc076
Leon Blakey <lord.quackstar@gmail.com>
2011-03-06 12:47:50 -0500

    Moved org.quackbot.info to org.quackbot.data

--- core/src/main/java/org/quackbot/data/Channel.java
+++ core/src/main/java/org/quackbot/data/Channel.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.quackbot.info;
+package org.quackbot.data;
 
 import org.quackbot.Controller;
 import org.slf4j.LoggerFactory;
