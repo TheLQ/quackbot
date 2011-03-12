33ac49b5d167a7565aeed058e503483b01ed3697
Leon Blakey <lord.quackstar@gmail.com>
2011-03-12 00:48:34 -0500

    Completely ported CoreQuackbotHook onMessage and setupCommand to new system

--- core/src/main/java/org/quackbot/CoreQuackbotHook.java
+++ core/src/main/java/org/quackbot/CoreQuackbotHook.java
@@ -7,12 +7,16 @@ import java.util.List;
 import java.util.Set;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
+import org.pircbotx.Channel;
+import org.pircbotx.User;
 import org.pircbotx.hooks.events.ConnectEvent;
+import org.pircbotx.hooks.events.MessageEvent;
 import org.quackbot.data.ChannelStore;
 import org.quackbot.err.AdminException;
 import org.quackbot.err.InvalidCMDException;
 import org.quackbot.err.NumArgException;
 import org.quackbot.hook.Hook;
+import org.quackbot.hook.HookManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,10 +25,7 @@ import org.slf4j.LoggerFactory;
  * @author Owner
  */
 public class CoreQuackbotHook extends Hook {
-	public CoreQuackbotHook() {
-		super("CoreQuackBotHooks");
-	}
-	private Logger log = LoggerFactory.getLogger(Bot.class);
+	private final Logger log = LoggerFactory.getLogger(Bot.class);
 
 	@Override
 	public void onConnect(ConnectEvent event) {
@@ -36,34 +37,40 @@ public class CoreQuackbotHook extends Hook {
 	}
 
 	@Override
-	public void onMessage(String channel, String sender, String login, String hostname, String message) {
-		int cmdNum = getController().addCmdNum();
-
-		String command = "";
-
-		if (getBot().isLocked(channel, sender, true)) {
+	public void onMessage(MessageEvent event) {
+		if (getBot().isLocked(event.getChannel(), event.getUser())) {
 			log.warn("Bot locked");
 			return;
 		}
-
+		
+		String message = event.getMessage();
+		
 		//Look for a prefix
 		for (String curPrefix : getBot().getPrefixes())
-			if (curPrefix.length() < message.length() && message.substring(0, curPrefix.length()).equalsIgnoreCase(curPrefix))
-				try {
-					log.info("-----------Begin execution of command #" + cmdNum + ",  from channel " + channel + " using message " + message + "-----------");
-					message = message.substring(curPrefix.length(), message.length()).trim();
-					command = message.split(" ", 2)[0];
-					BaseCommand cmd = setupCommand(command, channel, sender, login, hostname, message);
-					getBot().sendMessage(channel, sender, cmd.onCommandGiven(channel, sender, login, hostname, getArgs(message)));
-					getBot().sendMessage(channel, sender, cmd.onCommandChannel(channel, sender, login, hostname, getArgs(message)));
-					getBot().sendMessage(channel, sender, executeOnCommand(cmd, getArgs(message)));
-					break;
-				} catch (Exception e) {
-					log.error("Error encountered when running command " + command, e);
-					getBot().sendMessage(channel, sender, "ERROR: " + e.getMessage());
-				} finally {
-					log.info("-----------End execution of command #" + cmdNum + ",  from channel " + channel + " using message " + message + "-----------");
-				}
+			//Strip away start of message and end when the message doesn't match anymore (meaning something changed)
+			if (!(message = StringUtils.removeStartIgnoreCase(message, curPrefix)).equals(message))
+				break;
+		
+		if(message.equals(event.getMessage()))
+			//Message didn't change, meaning no prefix. Ignore
+			return;
+		
+		int commandNumber = getController().addCommandNumber();
+		String command = "";
+		
+		try {
+			log.info("-----------Begin execution of command #" + commandNumber + ",  from channel " + event.getChannel().getName() + " using message " + message + "-----------");
+			command = message.split(" ", 2)[0];
+			Command cmd = setupCommand(message, command, event.getChannel(), event.getUser());
+			getBot().sendMessage(event.getChannel(), event.getUser(), cmd.onCommand(event.getChannel(), event.getUser(), getArgs(message)));
+			getBot().sendMessage(event.getChannel(), event.getUser(), cmd.onCommandChannel(event.getChannel(), event.getUser(), getArgs(message)));
+			getBot().sendMessage(event.getChannel(), event.getUser(), executeOnCommand(cmd, getArgs(message)));
+		} catch (Exception e) {
+			log.error("Error encountered when running command " + command, e);
+			getBot().sendMessage(event.getChannel(), event.getUser(), "ERROR: " + e.getMessage());
+		} finally {
+			log.info("-----------End execution of command #" + commandNumber + ",  from channel " + event.getChannel() + " using message " + message + "-----------");
+		}
 	}
 
 	@Override
@@ -123,27 +130,27 @@ public class CoreQuackbotHook extends Hook {
 		return args;
 	}
 
-	public BaseCommand setupCommand(String command, String channel, String sender, String login, String hostname, String message) throws Exception {
+	public Command setupCommand(String message, String userCommand, Channel chan, User user) throws Exception {
 		//Parse message to get cmd and args
 		String[] args = getArgs(message);
 
-		BaseCommand plugin = CommandManager.getCommand(command);
-		//Is this a valid plugin?
-		if (plugin == null || !plugin.isEnabled())
-			throw new InvalidCMDException(command);
+		Command command = HookManager.getCommand(userCommand);
+		//Is this a valid command?
+		if (command == null || !command.isEnabled())
+			throw new InvalidCMDException(userCommand);
 		//Is this an admin function? If so, is the person an admin?
-		if (plugin.isAdmin() && !getController().isAdmin(sender, getBot(), channel))
+		if (command.isAdmin() && !getController().isAdmin(getBot(), user, chan))
 			throw new AdminException();
 
 		//Does the required number of args exist?
 		int given = args.length;
-		int required = plugin.getRequiredParams();
-		int optional = plugin.getOptionalParams();
+		int required = command.getRequiredParams();
+		int optional = command.getOptionalParams();
 		log.debug("User Args: " + given + " | Req Args: " + required + " | Optional: " + optional);
 		if (given > required + optional) //Do we have too many?
 			throw new NumArgException(given, required, optional);
 		else if (given < required) //Do we not have enough?
 			throw new NumArgException(given, required);
-		return plugin;
+		return command;
 	}
 }
