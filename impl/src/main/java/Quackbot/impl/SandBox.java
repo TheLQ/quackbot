6198f94a97c500e122c10a4be2e502d66f99bdff
Leon Blakey <lord.quackstar@gmail.com>
2011-03-13 16:17:46 -0400

    Use correct year and correct project name in License headers

--- impl/src/main/java/Quackbot/impl/SandBox.java
+++ impl/src/main/java/Quackbot/impl/SandBox.java
@@ -1,23 +1,37 @@
 /**
- * Copyright (C) 2010 Leon Blakey <lord.quackstar at gmail.com>
+ * Copyright (C) 2011 Leon Blakey <lord.quackstar at gmail.com>
  *
- * This file is part of PircBotX.
+ * This file is part of Quackbot.
  *
- * PircBotX is free software: you can redistribute it and/or modify
+ * Quackbot is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * PircBotX is distributed in the hope that it will be useful,
+ * Quackbot is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with PircBotX.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Quackbot.  If not, see <http://www.gnu.org/licenses/>.
  */
 package Quackbot.impl;
 
-public class SandBox {
-}
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import org.quackbot.hooks.JSHookLoader;
 
+public class SandBox {
+	public static void main(String... args) {
+		try {
+			Enumeration<URL> resources = JSHookLoader.class.getClassLoader().getResources("");
+			while (resources.hasMoreElements())
+				System.out.println(resources.nextElement());
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		System.out.println("End");
+	}
+}
