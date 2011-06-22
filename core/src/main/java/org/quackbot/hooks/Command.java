c864f5af45e0fc568633737a063b5dc68e8bb516
Leon Blakey <lord.quackstar@gmail.com>
2011-06-22 15:17:15 -0400

    Moved command to org.quackbot.hooks package

--- core/src/main/java/org/quackbot/hooks/Command.java
+++ core/src/main/java/org/quackbot/hooks/Command.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Quackbot.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.quackbot;
+package org.quackbot.hooks;
 
 import java.io.File;
 import lombok.EqualsAndHashCode;
