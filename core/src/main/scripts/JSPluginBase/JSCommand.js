f1b1bd671c26a72d7e8bf728da57c5edaa1f0a20
Leon Blakey <lord.quackstar@gmail.com>
2011-06-30 10:41:24 -0400

    Added helpful documentation to JSCommand

--- core/src/main/scripts/JSPluginBase/JSCommand.js
+++ core/src/main/scripts/JSPluginBase/JSCommand.js
@@ -21,9 +21,11 @@ var name, help = "", admin = false, enabled = true, file, alreadySetup = false;
 function getRequiredParams() {
 	var specified = 0;
 	var onCommandSpec = QuackUtils.onCommandParse(onCommand);
-	if(typeof(parameters) == 'object') {
+	if(typeof parameters == 'object') {
+		//See if its an array, declared with [5, 0]
 		if(QuackUtils.isArray(parameters))
 			specified = parameters[0]
+		//See if its an object, declared with {required: 5, optional: 0}
 		else if(typeof parameters.required != 'undefined')
 			specified =  parameters.required;
 	}
@@ -35,7 +37,7 @@ function getRequiredParams() {
 	optional = getOptionalParams();
 	if(specified != 0) //If manually specified, return it
 		return specified;
-	else if(optional != 0 && onCommandSpec != 0) //If there is an onCommand and optional, subtract it out
+	else if(optional != 0 && optional != -1 && onCommandSpec != 0) //If only optional is specified, use that
 		return onCommandSpec - optional;
 	//Only left with onCommand or 0
 	return onCommandSpec;
@@ -44,8 +46,10 @@ function getRequiredParams() {
 function getOptionalParams() {
 	var specified = 0;
 	if(typeof(parameters) == 'object')
+		//See if its an array, declared with [5, 0]
 		if(QuackUtils.isArray(parameters))
 			specified = parameters[1]
+		//See if its an object, declared with {required: 5, optional: 0}
 		else if(typeof parameters.optional != 'undefined')
 			specified = parameters.optional;
 	//You can't label individual params as optional, so it can only be specified
