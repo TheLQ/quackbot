230e8b39e57c70dda134a877275e241d22fae86e
Leon Blakey <lord.quackstar@gmail.com>
2011-07-10 22:56:54 -0400

    Require some fields to be set

--- store-hibernate/src/main/java/org/quackbot/dao/hibernate/UserChannelHb.java
+++ store-hibernate/src/main/java/org/quackbot/dao/hibernate/UserChannelHb.java
@@ -20,7 +20,6 @@ package org.quackbot.dao.hibernate;
 
 import java.io.Serializable;
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -28,7 +27,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
