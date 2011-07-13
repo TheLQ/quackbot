00c056efabf16a7fd95988a56abc42030d8a2181
Leon Blakey <lord.quackstar@gmail.com>
2011-07-13 00:51:58 -0400

    Moved the resposibility of adding users to a channel to the test instead of the generate method

--- store-hibernate/src/test/java/org/quackbot/dao/hibernate/GenericHbTest.java
+++ store-hibernate/src/test/java/org/quackbot/dao/hibernate/GenericHbTest.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright (C) 2011 Leon Blakey <lord.quackstar at gmail.com>
+ *
+ * This file is part of Quackbot.
+ *
+ * Quackbot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Quackbot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Quackbot.  If not, see <http://www.gnu.org/licenses/>.
+ */
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -44,9 +62,6 @@ public class GenericHbTest {
 	protected ChannelDAOHb generateChannel() {
 		ChannelDAOHb channel = new ChannelDAOHb();
 		channel.setName("#channelName");
-		channel.getUsers().add(generateUser("someNickNormal"));
-		channel.getOps().add(generateUser("someNickOp"));
-		channel.getVoices().add(generateUser("someNickVoice"));
 		return channel;
 	}
 
