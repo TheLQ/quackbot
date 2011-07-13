01133a7d020fac88c2ad97492eb468cc143d0343
Leon Blakey <lord.quackstar@gmail.com>
2011-07-13 00:59:58 -0400

    Make sure channel requires a server

---
+++ store-hibernate/src/test/java/org/quackbot/dao/hibernate/FailureTest.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C) 2011 Leon Blakey <lord.quackstar at gmail.com>
+ *
+ * This file is part of Quackbot.
+ *
+ * Quackbot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Quackbot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Quackbot.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.quackbot.dao.hibernate;
+
+import org.hibernate.PropertyValueException;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author lordquackstar
+ */
+public class FailureTest extends GenericHbTest {
+	@Test(expectedExceptions = PropertyValueException.class, description = "Makes sure server is required on channel")
+	public void channelServerRequiredTest() {
+		//Generate a channel without a server
+		ChannelDAOHb channel = generateChannel("#aChannel");
+		session.save(channel);
+	}
+}
