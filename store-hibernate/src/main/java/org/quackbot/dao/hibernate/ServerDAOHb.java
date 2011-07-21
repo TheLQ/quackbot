4e4075a3d39931b2a5bdcbc588632b197222bd0d
Leon Blakey <lord.quackstar@gmail.com>
2011-07-21 18:59:06 -0400

    Another update to store-hibernate that adds delete functionality and fixes some cascade issues

--- store-hibernate/src/main/java/org/quackbot/dao/hibernate/ServerDAOHb.java
+++ store-hibernate/src/main/java/org/quackbot/dao/hibernate/ServerDAOHb.java
@@ -20,6 +20,7 @@ package org.quackbot.dao.hibernate;
 
 import java.io.Serializable;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
@@ -36,6 +37,7 @@ import javax.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.hibernate.Session;
 import org.quackbot.dao.AdminDAO;
 import org.quackbot.dao.ChannelDAO;
 import org.quackbot.dao.ServerDAO;
@@ -55,40 +57,40 @@ public class ServerDAOHb implements ServerDAO, Serializable {
 	private Integer port;
 	private String password;
 	private Set<ChannelDAO> channels = new ListenerSet<ChannelDAO>(new HashSet()) {
-			@Override
-			public void onAdd(ChannelDAO entry) {
-				entry.setServer(ServerDAOHb.this);
-			}
+		@Override
+		public void onAdd(ChannelDAO entry) {
+			entry.setServer(ServerDAOHb.this);
+		}
 
-			@Override
-			public void onRemove(Object entry) {
-				if (!(entry instanceof ChannelDAO))
-					throw new RuntimeException("Attempting to remove unknown object from server channel list " + entry);
-				//Do nothing
-			}
-		};
+		@Override
+		public void onRemove(Object entry) {
+			if (!(entry instanceof ChannelDAO))
+				throw new RuntimeException("Attempting to remove unknown object from server channel list " + entry);
+			//Do nothing
+		}
+	};
 	private Set<AdminDAO> admins = new ListenerSet<AdminDAO>(new HashSet()) {
-			@Override
-			public void onAdd(AdminDAO entry) {
-				entry.getServers().add(ServerDAOHb.this);
-			}
+		@Override
+		public void onAdd(AdminDAO entry) {
+			entry.getServers().add(ServerDAOHb.this);
+		}
 
-			@Override
-			public void onRemove(Object entry) {
-				if (!(entry instanceof AdminDAOHb))
-					throw new RuntimeException("Attempting to remove unknown object from server admin list " + entry);
-				//Do nothing
-			}
-		};
+		@Override
+		public void onRemove(Object entry) {
+			if (!(entry instanceof AdminDAOHb))
+				throw new RuntimeException("Attempting to remove unknown object from server admin list " + entry);
+			//Do nothing
+		}
+	};
 
 	public ServerDAOHb() {
 	}
 
-	@ManyToMany(cascade = CascadeType.ALL, targetEntity = AdminDAOHb.class)
+	@ManyToMany(cascade= CascadeType.ALL, targetEntity = AdminDAOHb.class)
 	@JoinTable(name = "quackbot_server_admins", joinColumns = {
 		@JoinColumn(name = "SERVER_ID")}, inverseJoinColumns = {
 		@JoinColumn(name = "ADMIN_ID")})
-	@org.hibernate.annotations.Cascade({org.hibernate.annotations.CascadeType.ALL, org.hibernate.annotations.CascadeType.DELETE})
+	//@org.hibernate.annotations.Cascade({org.hibernate.annotations.CascadeType.ALL, org.hibernate.annotations.CascadeType.DELETE})
 	public Set<AdminDAO> getAdmins() {
 		return admins;
 	}
@@ -124,7 +126,56 @@ public class ServerDAOHb implements ServerDAO, Serializable {
 
 	@Override
 	public boolean delete() {
+		Session session = DAOControllerHb.getInstance().getSession();
+		for(ChannelDAO channel : channels)
+			channel.setServer(this);
+		for(AdminDAO admin : admins) {
+			admin.getServers().remove(this);
+		}
+		session.delete(this);
+
+		if (true)
+			return true;
+
+		//Delete all channels manually (clone the set so a ConcurrentModificationException isn't raised as channel deletes itself)
+		//for(ChannelDAO channel : new HashSet<ChannelDAO>(channels))
+		//	channel.delete();
+		System.out.println("--- BEGIN DELETING USER MAPS ---");
+		session.createQuery("delete from UserChannelHb where USER_ID in (from UserDAOHb where SERVER_ID = :server_id)").setInteger("server_id", serverId).executeUpdate();
+		System.out.println("--- BEGIN DELETING USERS ---");
+		session.createQuery("delete from UserDAOHb where SERVER_ID = :server_id").setInteger("server_id", serverId).executeUpdate();
+		System.out.println("--- BEGIN DELETING CHANNELS ---");
+		channels.clear();
+		System.out.println("--- BEGIN DELETING ADMINS ---");
+
+
+		//session.createQuery("delete from UserChannelHb where USER_ID in (from UserDAOHb where SERVER_ID = :server_id)").setInteger("server_id", serverId).executeUpdate();
+		//session.refresh(this);
+		//channels.clear();
+
+		//Remove all admins
+		//for(Iterator<AdminDAO> itr = admins.iterator(); itr.hasNext();) {
+		//	AdminDAO curAdmin = itr.next();
+		//	curAdmin.getServers().remove(this);
+		//	itr.remove();
+		//}
+		admins.clear();
+
+		/*
+		for (Iterator<AdminDAO> itr = admins.iterator(); itr.hasNext();) {
+		AdminDAO curAdmin = itr.next();
+		curAdmin.getServers().remove(this);
+		itr.remove();
+		
+		if(curAdmin.getName().equals("globalAdmin")) {
+		
+		}
+		ClassMetadata metadata = session.getSessionFactory().getClassMetadata(AdminDAOHb.class);
+		System.out.println("Identifyier for admin " + curAdmin.getName() + ": " + metadata.getIdentifier(curAdmin, (SessionImplementor) session));
+		}*/
+		System.out.println("--- BEGIN DELETING SERVER ---");
 		DAOControllerHb.getInstance().getSession().delete(this);
+		System.out.println("--- DELETE FINISHED ---");
 		return true;
 	}
 }
