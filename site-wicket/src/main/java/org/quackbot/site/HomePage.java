36ec18b4e40ac4aabae0ca218ddbe2289b7650ff
Leon Blakey <lord.quackstar@gmail.com>
2011-07-22 02:34:33 -0400

    Created basic website layout

--- site-wicket/src/main/java/org/quackbot/site/HomePage.java
+++ site-wicket/src/main/java/org/quackbot/site/HomePage.java
@@ -19,17 +19,14 @@
 package org.quackbot.site;
 
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
 
 /**
- * Homepage
+ *
+ * @author lordquackstar
  */
-public class HomePage extends WebPage {
-
-	private static final long serialVersionUID = 1L;
-
-	// TODO Add any page properties or variables here
+public class HomePage extends QuackbotPage {
 
     /**
 	 * Constructor that is invoked when page is invoked without a session.
@@ -38,10 +35,8 @@ public class HomePage extends WebPage {
 	 *            Page parameters
 	 */
     public HomePage(final PageParameters parameters) {
-
+		super(parameters);
         // Add the simplest type of label
-        add(new Label("message", "If you see this message wicket is properly configured and running"));
-
-        // TODO Add your page's components here
+        add(new Label("label1", "Home page content"));
     }
 }
