2d38fd36f72a78879007c9dfd80b7f005a1f0af9
Leon Blakey <lord.quackstar@gmail.com>
2011-07-22 17:05:45 -0400

    Updated left over core hooks to use new getController method

--- core/src/main/java/org/quackbot/hooks/core/AdminHelp.java
+++ core/src/main/java/org/quackbot/hooks/core/AdminHelp.java
@@ -41,7 +41,7 @@ public class AdminHelp extends Command {
 			List<String> cmdList = new ArrayList<String>();
 
 			//Add Java Plugins
-			for (Command curCmd : getController().getHookManager().getCommands())
+			for (Command curCmd : getController(event).getHookManager().getCommands())
 				if (curCmd.isEnabled() && curCmd.isAdmin())
 					cmdList.add(curCmd.getName());
 
@@ -50,7 +50,7 @@ public class AdminHelp extends Command {
 		}
 		
 		//Command specified, get specific help for it
-		Command result = getController().getHookManager().getCommand(command);
+		Command result = getController(event).getHookManager().getCommand(command);
 		if (result == null)
 			throw new InvalidCMDException(command);
 		else if (!result.isEnabled())
