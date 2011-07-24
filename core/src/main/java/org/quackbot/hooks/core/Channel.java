4b1f45380fbeaae7cb640643833283af95d150bb
Leon Blakey <lord.quackstar@gmail.com>
2011-07-23 20:13:40 -0400

    Moved the target (what all subcommands use) to a dedicated parameter

--- core/src/main/java/org/quackbot/hooks/core/Channel.java
+++ core/src/main/java/org/quackbot/hooks/core/Channel.java
@@ -1,13 +1,13 @@
 package org.quackbot.hooks.core;
 
-import javax.management.RuntimeErrorException;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.quackbot.Bot;
-import org.quackbot.err.QuackbotException;
 import org.quackbot.events.CommandEvent;
 import org.quackbot.hooks.Command;
 import org.quackbot.hooks.java.AdminOnly;
 import org.quackbot.hooks.java.HelpDoc;
+import org.quackbot.hooks.java.Optional;
 
 /**
  *
@@ -16,113 +16,113 @@ import org.quackbot.hooks.java.HelpDoc;
 @AdminOnly
 @HelpDoc(value = "The umbrilla command for any action to a channel")
 public class Channel extends Command {
-	public String onCommand(CommandEvent event, String action, String[] args) throws Exception {
+	public String onCommand(CommandEvent event, String action, String target, @Optional String[] arg2) throws Exception {
 		Bot bot = event.getBot();
 
 		/*** Bot status commands ***/
 		if (action.equalsIgnoreCase("join")) {
-			if (args.length == 0)
+			if (StringUtils.isBlank(target))
 				throw new RuntimeException("No channel specified");
-			bot.joinChannel(args[0]);
+			bot.joinChannel(target);
 		} else if (action.equalsIgnoreCase("part"))
-			if (args.length == 0)
+			if (StringUtils.isBlank(target))
 				//No args, assume current channel
 				bot.partChannel(event.getChannel());
 			else {
 				//Want to part other channel; make sure it exists
-				if (!bot.channelExists(args[0]))
-					throw new RuntimeException("Channel " + args[0] + " doesn't exist");
-				bot.partChannel(bot.getChannel(args[0]));
+				if (!bot.channelExists(target))
+					throw new RuntimeException("Channel " + target + " doesn't exist");
+				bot.partChannel(bot.getChannel(target));
 			}
 		else if (action.equalsIgnoreCase("quit"))
-			if (args.length == 0)
+			if (StringUtils.isBlank(target))
 				//No args = no quit message
 				bot.quitServer();
 			else
 				//Args, join to make quit message
-				bot.quitServer(StringUtils.join(args, " "));
+				bot.quitServer(StringUtils.join(ArrayUtils.add(arg2, 0, target), " "));
 		/**** Set user channel status commands *****/
 		else if (action.equalsIgnoreCase("op")) {
-			userArgCheck(event, args);
-			event.getChannel().op(bot.getUser(args[0]));
+			userArgCheck(event, target);
+			event.getChannel().op(bot.getUser(target));
 		} else if (action.equalsIgnoreCase("voice")) {
-			userArgCheck(event, args);
-			event.getChannel().voice(bot.getUser(args[0]));
+			userArgCheck(event, target);
+			event.getChannel().voice(bot.getUser(target));
 		} else if (action.equalsIgnoreCase("halfOp")) {
-			userArgCheck(event, args);
-			event.getChannel().halfOp(bot.getUser(args[0]));
+			userArgCheck(event, target);
+			event.getChannel().halfOp(bot.getUser(target));
 		} else if (action.equalsIgnoreCase("superOp")) {
-			userArgCheck(event, args);
-			event.getChannel().superOp(bot.getUser(args[0]));
+			userArgCheck(event, target);
+			event.getChannel().superOp(bot.getUser(target));
 		} else if (action.equalsIgnoreCase("owner")) {
-			userArgCheck(event, args);
-			event.getChannel().owner(bot.getUser(args[0]));
+			userArgCheck(event, target);
+			event.getChannel().owner(bot.getUser(target));
 		} /*** Remove user channel status commands ***/
 		else if (action.equalsIgnoreCase("deop")) {
-			userArgCheck(event, args);
-			event.getChannel().deOp(bot.getUser(args[0]));
+			userArgCheck(event, target);
+			event.getChannel().deOp(bot.getUser(target));
 		} else if (action.equalsIgnoreCase("devoice")) {
-			userArgCheck(event, args);
-			event.getChannel().deVoice(bot.getUser(args[0]));
+			userArgCheck(event, target);
+			event.getChannel().deVoice(bot.getUser(target));
 		} else if (action.equalsIgnoreCase("dehalfOp")) {
-			userArgCheck(event, args);
-			event.getChannel().deHalfOp(bot.getUser(args[0]));
+			userArgCheck(event, target);
+			event.getChannel().deHalfOp(bot.getUser(target));
 		} else if (action.equalsIgnoreCase("desuperOp")) {
-			userArgCheck(event, args);
-			event.getChannel().deSuperOp(bot.getUser(args[0]));
+			userArgCheck(event, target);
+			event.getChannel().deSuperOp(bot.getUser(target));
 		} else if (action.equalsIgnoreCase("deowner")) {
-			userArgCheck(event, args);
-			event.getChannel().deOwner(bot.getUser(args[0]));
+			userArgCheck(event, target);
+			event.getChannel().deOwner(bot.getUser(target));
 		} /*** Other user commands ***/
 		else if (action.equalsIgnoreCase("ban") || action.equalsIgnoreCase("kickban"))
-			if (args[0].contains("!"))
+			if (target.contains("!"))
 				//Contains a !, assume hostmask
-				bot.ban(event.getChannel(), args[0]);
+				bot.ban(event.getChannel(), target);
 			else {
 				//Must be a user
-				userArgCheck(event, args);
-				bot.ban(event.getChannel(), "*!*@" + bot.getUser(args[0]).getHostmask());
+				userArgCheck(event, target);
+				bot.ban(event.getChannel(), "*!*@" + bot.getUser(target).getHostmask());
 			}
 		else if (action.equalsIgnoreCase("unban"))
-			if (args[0].contains("!"))
+			if (target.contains("!"))
 				//Contains a !, assume hostmask
-				bot.unBan(event.getChannel(), args[0]);
+				bot.unBan(event.getChannel(), target);
 			else {
 				//Must be a user
-				userArgCheck(event, args);
-				bot.unBan(event.getChannel(), "*!*@" + bot.getUser(args[0]).getHostmask());
+				userArgCheck(event, target);
+				bot.unBan(event.getChannel(), "*!*@" + bot.getUser(target).getHostmask());
 			}
 		else if (action.equalsIgnoreCase("tempBan")) {
-			if(args.length != 2)
+			if(arg2.length != 1)
 				throw new RuntimeException("Not enough arguments. Should be <hostmask or user> <time in minuites>");
-			int banTime = Integer.parseInt(args[1]);
+			int banTime = Integer.parseInt(arg2[0]);
 			String hostmask;
-			if (args[0].contains("!"))
+			if (target.contains("!"))
 				//Contains a !, assume hostmask
-				hostmask = args[0];
+				hostmask = target;
 			else {
 				//Must be a user
-				userArgCheck(event, args);
-				hostmask = "*!*@" + bot.getUser(args[0]).getHostmask();
+				userArgCheck(event, target);
+				hostmask = "*!*@" + bot.getUser(target).getHostmask();
 			}
 			bot.ban(event.getChannel(), hostmask);
 			Thread.sleep(banTime * 60 * 1000);
 			bot.unBan(event.getChannel(), hostmask);
 		} else if (action.equalsIgnoreCase("kick") || action.equalsIgnoreCase("kickban")) {
-			userArgCheck(event, args);
-			bot.kick(event.getChannel(), bot.getUser(args[0]));
+			userArgCheck(event, target);
+			bot.kick(event.getChannel(), bot.getUser(target));
 		} else
 			throw new RuntimeException("Unknown operation: " + action);
 		return null;
 	}
 
-	protected void userArgCheck(CommandEvent event, String[] args) {
+	protected void userArgCheck(CommandEvent event, String user) {
 		Bot bot = event.getBot();
-		if (args.length == 0)
+		if (StringUtils.isBlank(user))
 			throw new RuntimeException("No user specified");
-		if (bot.userExists(args[0]))
-			throw new RuntimeException("User " + args[0] + " doesn't exist");
-		if (!event.getChannel().getUsers().contains(bot.getUser(args[0])))
-			throw new RuntimeException("User " + args[0] + " doesn't exist this channel (" + event.getChannel().getName() + ")");
+		if (bot.userExists(user))
+			throw new RuntimeException("User " + user + " doesn't exist");
+		if (!event.getChannel().getUsers().contains(bot.getUser(user)))
+			throw new RuntimeException("User " + user + " doesn't exist this channel (" + event.getChannel().getName() + ")");
 	}
 }
