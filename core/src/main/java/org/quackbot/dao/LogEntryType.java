5a8ff75588bef91ab2fb38efd5b3e0059e81e9e3
Leon Blakey <lord.quackstar@gmail.com>
2011-07-27 19:54:02 -0400

    Use new enum LogEntryType specifying LogEntryDAO types instead of just passing a String

---
+++ core/src/main/java/org/quackbot/dao/LogEntryType.java
@@ -0,0 +1,15 @@
+
+package org.quackbot.dao;
+
+/**
+ * Types of allowed Log Entries.
+ * @author Leon Blakey <lord.quackstar at gmail.com>
+ */
+public enum LogEntryType {
+	MESSAGE,
+	PRIVATE_MESSAGE,
+	CHANNEL_MODE,
+	JOIN,
+	QUIT,
+	PART
+}
