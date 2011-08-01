43a5abf7bc6e6327a7cd19c3dddb4b066fed592b
Leon Blakey <lord.quackstar@gmail.com>
2011-08-01 00:14:30 -0400

    Use generics on all get methods in the DAO objects

--- core/src/main/java/org/quackbot/dao/AdminDAO.java
+++ core/src/main/java/org/quackbot/dao/AdminDAO.java
@@ -7,7 +7,7 @@ import org.quackbot.dao.model.AdminEntry;
  * @author Leon Blakey <lord.quackstar at gmail.com>
  */
 public interface AdminDAO<T extends AdminEntry> extends GenericDAO<T> {
-	public AdminEntry findByName(String adminName);
+	public T findByName(String adminName);
 
-	public AdminEntry create(String adminName);
+	public T create(String adminName);
 }
