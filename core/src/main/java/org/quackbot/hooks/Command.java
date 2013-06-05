baabdb06910095d8151bbeee24cc2498eec9bc2b
Leon Blakey <lord.quackstar@gmail.com>
2013-06-05 18:15:46 -0400

    Command is now an interface that describes features of the command

--- core/src/main/java/org/quackbot/hooks/Command.java
+++ core/src/main/java/org/quackbot/hooks/Command.java
@@ -18,72 +18,30 @@
  */
 package org.quackbot.hooks;
 
-import lombok.AccessLevel;
+import com.google.common.collect.ImmutableList;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Setter;
-import org.quackbot.err.QuackbotException;
-import org.quackbot.events.CommandEvent;
+import org.quackbot.AdminLevel;
 
 /**
  *
  * @author Leon Blakey <lord.quackstar at gmail.com>
  */
-@Data
-@EqualsAndHashCode(callSuper = true)
-@Setter(AccessLevel.NONE)
-public abstract class Command extends Hook {
-	private String help = "";
-	private boolean admin = false;
-	@Setter
-	private boolean enabled = true;
-	private int requiredParams = 0;
-	private int optionalParams = 0;
-	private boolean setup;
-
-	public Command() {
-		super();
+public interface Command {
+	public String getName();
+	public String getHelp();
+	public AdminLevel getMinimumAdminLevel();
+	public ImmutableList<Argument> getArguments();
+	
+	public static interface Argument {
+		public String getName();
+		public String getArgumentHelp();
+		public boolean isRequired();
 	}
 	
-	/**
-	 * Create a Command with the given name. File is null
-	 * @param name The name to use for this Command
-	 */
-	public Command(String name) {
-		super(name);
-	}
-
-	/**
-	 * Create a Command with the given file and name
-	 * @param fileLocation The file that this Command came from
-	 * @param name The name to use for this Command
-	 */
-	public Command(String fileLocation, String name) {
-		super(fileLocation, name);
-	}
-	
-	public void setup(String help, boolean admin, boolean enabled, int requiredParams, int optionalParams) throws QuackbotException {
-		if(setup)
-			throw new QuackbotException("This hook has already been setup");
-		this.help = help;
-		this.admin = admin;
-		this.enabled = enabled;
-		this.requiredParams = requiredParams;
-		this.optionalParams = optionalParams;
-	}
-
-	/**
-	 * Called when invoked in a channel or private message
-	 * @param chan The channel the command may of taken place in. <b>Null means
-	 *             this command was invoked in a PM</b>
-	 * @param user The user that invoked this command
-	 * @param args Any arguments the user gave, presented as an array for ease
-	 *             of use
-	 * @return A response to the user's command. Can be null
-	 * @throws Exception Any exception you encountered <i>needs</i> to be thrown
-	 *                   and not handled internally
-	 */
-	public String onCommand(CommandEvent event) throws Exception {
-		return null;
+	@Data
+	public static class VarargArgument implements Argument {
+		protected final String name;
+		protected final String argumentHelp;
+		protected final boolean required = true;
 	}
 }
