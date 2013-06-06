c09846a932f02b738079389ecdfd600d2858c55a
Leon Blakey <lord.quackstar@gmail.com>
2013-06-05 20:15:00 -0400

    Overhauled Java command processing with simple annotations and clearer loading

---
+++ core/src/main/java/org/quackbot/hooks/java/JavaArgument.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.quackbot.hooks.java;
+
+/**
+ * Implementation of {@link org.quackbot.hooks.Command.Argument} in annotation form.
+ * Methods are copied and pasted since annotations cannot extend interfaces for some reason
+ * @author Leon
+ */
+public @interface JavaArgument {
+	public String value();
+
+	public String getArgumentHelp() default "";
+
+	public boolean isRequired() default true;
+}
